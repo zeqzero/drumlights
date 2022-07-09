7bd2bdcc72e4168fe471779b79b409e217bfcdb3
zeqzero <willis.thomas.j@gmail.com>
2022-07-09 16:52:43 -0700

    Update control_arduino.py

--- control_arduino.py
+++ control_arduino.py
@@ -7,6 +7,6 @@ usb = serial.Serial(USB_PORT, 115200, timeout=2)
 while True:
     print('.')
     usb.write(b'led_on')
-    time.sleep(0.25)
+    time.sleep(1)
     usb.write(b'led_off')
-    time.sleep(0.25)
+    time.sleep(1)
