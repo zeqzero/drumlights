b733a5638d04d85941b333eebe830cd6f956ab56
zeqzero <willis.thomas.j@gmail.com>
2022-07-09 11:13:10 -0700

    Update drumlights.py

--- pi/drumlights.py
+++ pi/drumlights.py
@@ -1,4 +1,4 @@
-import mido, board, neopixel, random
+import mido, board, neopixel, random, time
 from colour import Color
 
 def ftoi(float_tuple):
@@ -8,32 +8,72 @@ class Lightstrip:
     def __init__(self, name, pixel_pin, pixel_count, color):
         self.name, self.pixel_pin, self.pixel_count, self.color = name, pixel_pin, pixel_count, color
         self.pixels = neopixel.NeoPixel(pixel_pin, pixel_count)
+        self.pulse_duration = 5.0
+        self.pulse_timer = 0.0
+        self.turn_on(0.5)
 
     def light(self, color):
         self.pixels.fill(ftoi(color.rgb))
 
-    def turn_on(self):
-        self.light(self.color)
+    def turn_on(self, value):
+        c = self.color
+        c.value = 0.5
+        self.light(c)
+
+    def depulse(self, value=0.5):
+        self.turn_on(value)
+        self.pulse_timer = 0.0
+
+    def pulse(self, value=1.0):
+        c = self.color
+        c.value = value
+        self.light(c)
+        self.pulse_timer = self.pulse_duration
 
     def turn_off(self):
         self.pixels.fill(ftoi(Color('black').rgb))
 
+    def update(self, t):
+        c = self.color
+        if self.pulse_timer > 0.0:
+            self.pulse_timer -= t
+        else:
+            self.depulse()
+
+class Drumlights:
+    def __init__(self):
+        self.setup_midi()
+        self.setup_lightstrips()
+
+    def setup_midi(self):
+        self.midi_device_name = [m for m in mido.get_input_names() if 'USB Midi Cable' in m][0]
+        self.midi_inport = mido.open_input(self.midi_device_name)
+
+    def setup_lightstrips(self):
+        self.lightstrips = (
+            Lightstrip('test', board.D18, 5, Color('blue')),
+        )
+
+    def update_lightstrips(self, t):
+        for lightstrip in self.lightstrips:
+            lightstrip.update(t)
+
+    def run(self):
+        t = time.time()
+        while True:
+            messages = self.midi_inport.iter_pending()
+
+            for message in messages:
+                if message.type == 'note_on':
+                    print('-', end='')
+                    self.lightstrips[0].pulse()
+
+            t = t - time.time()
+            update_lightstrips(t)
+
 print('starting drumlights...')
 
-midi_device_name = [m for m in mido.get_input_names() if 'USB Midi Cable' in m][0]
-
-lightstrips = (
-    Lightstrip('test', board.D18, 5, Color('blue')),
-)
-
-with mido.open_input(midi_device_name) as inport:
-    while True:
-        for msg in inport:
-            if msg.type == 'note_on':
-                print('-', end='')
-                lightstrips[0].turn_on()
-            if msg.type == 'note_off':
-                print('_', end='')
-                lightstrips[0].turn_off()
+d = Drumlights()
+d.run()
 
 print ('exitting drumlights...')
