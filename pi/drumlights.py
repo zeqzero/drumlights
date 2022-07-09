7d26a492dcd02db2d96905f7e8346d713476b3d6
zeqzero <willis.thomas.j@gmail.com>
2022-07-08 17:51:32 -0700

    add neopixel

--- pi/drumlights.py
+++ pi/drumlights.py
@@ -1,12 +1,24 @@
-import mido
+import mido, board, neopixel, random
 
-print('starting...')
+class Lightstrip:
+    def __init__(self, name, pixel_pin, pixel_count):
+        self.name, self.pixel_pin, self.pixel_count = name, pixel_pin, pixel_count
+        self.pixels = neopixel.NeoPixel(pixel_pin, pixel_count)
+
+    def light(self, color):
+        self.pixels.fill(color)
+
+
+print('starting drumlights...')
 
 midi_device_name = 'USB Midi Cable:USB Midi Cable MIDI 1 24:0'
+
+lightstrips = (
+    Lightstrip('test', board.D18, 30)
+)
 
 with mido.open_input(midi_device_name) as inport:
     while True:
         for msg in inport:
-            print(msg)
-
-    
+            #print(msg)
+            lightstrips[0].light((random.randint(0,255), random.randint(0,255), random.randint(0,255)))
