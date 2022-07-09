2cf6f07ebc180185c324ec1baf2a61162a8ecfa2
zeqzero <willis.thomas.j@gmail.com>
2022-07-09 10:48:29 -0700

    add colour

--- pi/drumlights.py
+++ pi/drumlights.py
@@ -1,30 +1,39 @@
 import mido, board, neopixel, random
+from colour import Color
+
+def ftoi(float_tuple):
+    return [round(f * 255.0) for f in float_tuple]
 
 class Lightstrip:
-    def __init__(self, name, pixel_pin, pixel_count):
-        self.name, self.pixel_pin, self.pixel_count = name, pixel_pin, pixel_count
+    def __init__(self, name, pixel_pin, pixel_count, color):
+        self.name, self.pixel_pin, self.pixel_count, self.color = name, pixel_pin, pixel_count, color
         self.pixels = neopixel.NeoPixel(pixel_pin, pixel_count)
 
     def light(self, color):
-        self.pixels.fill(color)
+        self.pixels.fill(ftoi(color))
 
-red = (255, 0, 0)
-blue = (0, 255, 0)
-green = (0, 0, 255)
+    def turn_on(self):
+        self.light(self.color)
+
+    def turn_off(self):
+        self.pixels.fill(ftoi(Color('black')))
 
 print('starting drumlights...')
 
 midi_device_name = [m for m in mido.get_input_names() if 'USB Midi Cable' in m][0]
 
 lightstrips = (
-    Lightstrip('test', board.D18, 5),
+    Lightstrip('test', board.D18, 5, Color('blue')),
 )
 
 with mido.open_input(midi_device_name) as inport:
     while True:
         for msg in inport:
             if msg.type == 'note_on':
-                print('.', end='')
-                lightstrips[0].light(random.choice((red,blue,green)))
+                print('-', end='')
+                lightstrips[0].light()
+            if msg.type == 'note_off':
+                print('_', end='')
+                lightstrips[0].turn_off()
 
 print ('exitting drumlights...')
