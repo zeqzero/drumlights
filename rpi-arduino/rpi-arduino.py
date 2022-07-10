a153271ae3d7f2800e0eba512fbe6ba179421f9e
zeqzero <willis.thomas.j@gmail.com>
2022-07-09 17:24:31 -0700

    Update rpi-arduino.py

--- rpi-arduino/rpi-arduino.py
+++ rpi-arduino/rpi-arduino.py
@@ -1,7 +1,7 @@
 # Importing Libraries
 import serial
 import time
-arduino = serial.Serial(port='/dev/ttyUSB0', baudrate=115200, timeout=.1)
+arduino = serial.Serial(port='/dev/ttyUSB1', baudrate=115200, timeout=.1)
 def write_read(x):
     arduino.write(bytes(x, 'utf-8'))
     time.sleep(0.05)
